382b28e68fa6cd0ecd5c29bc10addb708d3f98f1
Paul Jaworski <paulmattjaws@gmail.com>
2019-09-07 11:58:34 -0700

    add styles to NoDataMsg

--- src/components/NoDataMsg.js
+++ src/components/NoDataMsg.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import colors from '../utils/colors';
+import Card from './Card';
 
 const StyledNoDataMsg = styled.div`
-  font-size: 2rem;
+  max-width: 660px;
+  margin: 0 auto;
+  font-size: 1.5rem;
   font-weight: bold;
-  color: ${colors.gray[9]};
+  color: ${colors.gray[7]};
+  text-align: center;
+  line-height: 1.2;
 `;
 
 const NoDataMsg = ({ resource }) => {
   return (
-    <StyledNoDataMsg>
-      Looks like you don't have any {resource} yet. Add one or import to get
-      started!
-    </StyledNoDataMsg>
+    <Card>
+      <StyledNoDataMsg>
+        Looks like you don't have any {resource} yet. Add one or import to get
+        started!
+      </StyledNoDataMsg>
+    </Card>
   );
 };
 
