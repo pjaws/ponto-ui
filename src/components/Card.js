23b7e657c5275843e4a7dd8ddc2f73cebd6007fe
Paul Jaworski <paulmattjaws@gmail.com>
2019-09-12 11:13:43 -0700

    add margin to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledCard = styled.div`
+  margin-bottom: 2rem;
+  padding: 2rem;
   border-radius: 0.25rem;
   background-color: #fff;
   box-shadow: 0 0 0 1px rgba(61, 61, 78, 0.05),
     0 1px 3px 0 rgba(61, 61, 78, 0.15);
-  padding: 2rem;
 `;
 
 const Card = ({ children }) => {
